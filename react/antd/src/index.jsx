ba20e8b1352aeca4819941dd3a2d058da8b6d7bf
redraiment <redraiment@gmail.com>
2021-12-31 17:03:00 +0800

    基于ant.design的react应用开发模板

---
+++ react/antd/src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import { Button } from 'antd';
+import 'antd/dist/antd.min.css';
+
+import './index.scss';
+
+ReactDOM.render(
+    <Button>Hello Ant Design</Button>,
+    document.getElementById('app')
+);
